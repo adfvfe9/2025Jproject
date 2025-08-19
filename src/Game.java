dd979fe2ea5b5811b6e1b25ab0f0f1a1828a7d2c
공은성 <adfvfe9@gmail.com>
2025-08-19 23:02:51 +0900

    20250819 - 3

--- src/Game.java
+++ src/Game.java
@@ -1,5 +1,31 @@
 public class Game {
     boolean start(int gamemode) {
+        switch (gamemode) {
+            case 1 : {
+
+            } break;
+            case 2 : {
+                feedLine(10);
+
+            } break;
+            case 3 : {
+                feedLine(10);
+                System.out.println("바꿀 키의 번호를 입력하세요.");
+                for (int i = 0; i < keys.length; i++) {
+                    System.out.println((i + 1) + ". " + keys[i] + " ----- " + selectkey[i]);
+                }
+                int changeKeyNum;
+                while (true) {
+                    changeKeyNum = Main.scan.nextInt();
+                    if (changeKeyNum >= 1 && changeKeyNum <= keys.length) {
+                        break;
+                    }
+                }
+                System.out.print(keys[changeKeyNum - 1] + " 키를 무슨 키로 바꾸시겠습니까? : ");
+                selectkey[changeKeyNum - 1] = Main.scan.next().charAt(0);
+                System.out.println(green("변경되었습니다!"));
+            }
+        }
         return true;
     }
     int startMenu() {
@@ -7,7 +33,7 @@ public class Game {
         int select = -1;
         int fsel = 0;
         boolean isSelect = false;
-        System.out.print("\033[H\033[2J");      // 걍 터미널 초기화
+        System.out.print("\033[H\033[2J");
         while (true) {
             System.out.print("\033[H\033[2J");
             if (!isSelect) {
@@ -15,13 +41,13 @@ public class Game {
                 for (String mode : modes) {
                     System.out.println(mode);
                 }
-            }
-            else {
+            } else {
                 feedLine(10);
-                System.out.println("s를 눌러 선택하세요.\n");
+                System.out.println(this.selectkey[0] + "를 눌러 선택하세요.\n");
             }
-            if (select > 3 || select < 1) System.err.println("1부터 3 사이의 값을 입력하세요.");
-            if (select <= 3 && select >= 1) {
+            if (select > 3 || select < 1)
+                System.err.println("1부터 3 사이의 값을 입력하세요.");
+            if (select >= 1 && select <= 3) {
                 for (int i = 0; i < modes.length; i++) {
                     if (select - 1 == i) {
                         System.out.println(red(modes[i]) + red(" <"));
@@ -31,11 +57,17 @@ public class Game {
                 }
                 fsel = select;
             }
-            select = Main.scan.next().charAt(0) - '0';
-            if (select != -1) isSelect = true;
-            if (select == 's' - '0') {
-                System.out.println(green("선택되었습니다!"));
-                return fsel;
+            String input = Main.scan.next();
+            if (input.length() == 1) {
+                char c = input.charAt(0);
+                if (c >= '1' && c <= '3') {
+                    select = c - '0';
+                    isSelect = true;
+                }
+                else if (isSelect && c == this.selectkey[0]) {
+                    System.out.println(green("선택되었습니다!"));
+                    return fsel;
+                }
             }
         }
     }
@@ -50,4 +82,6 @@ public class Game {
     String green(String s) {
         return "\u001B[32m" + s + "\u001B[0m";
     }
+    String keys[] = {"선택"};
+    static char selectkey[] = {'s'};   // 0.select
 }
--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         while (true) {
             Game game = new Game();
-            game.start(game.startMenu());
+            if (!game.start(game.startMenu())) break;
         }
     }
 }
